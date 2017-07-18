9b89af499b287f30117d9cffd5d452b7705b7fcd
Ubuntu <ubuntu@ip-172-31-24-186.ap-southeast-2.compute.internal>
2017-07-18 15:01:38 +0000

    Added shub deployment code

---
+++ build/lib.linux-x86_64-2.7/generic_crawler/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProductListItem(scrapy.Item):
+    # define the fields for your item here like:
+     producturl = scrapy.Field()
+     pass
+
+class ProductDetailItem(scrapy.Item):
+    # define the fields for your item here like:
+     url = scrapy.Field()
+     price = scrapy.Field()
+     name = scrapy.Field()
+     description = scrapy.Field()
+     sku = scrapy.Field()
+     rrp = scrapy.Field()
+     brand = scrapy.Field()
+     category = scrapy.Field()
+     site = scrapy.Field()
+     pass
---
+++ build/lib.linux-x86_64-2.7/generic_crawler/spiders/productlist.py
@@ -0,0 +1,50 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from generic_crawler.items import ProductListItem
+
+class ProductList(CrawlSpider):
+    name = 'productlist'
+    start_urls = [
+      	'https://www.awave.com.au/product-category/outboard/',
+	'https://www.awave.com.au/product-category/',
+        'https://www.awave.com.au/product-category/monitoring/',
+	'https://www.awave.com.au/product-category/consoles/',
+	'https://www.awave.com.au/product-category/instruments/',
+	'https://www.awave.com.au/product-category/computer-music/',
+	'https://www.awave.com.au/product-category/accessories/'
+	]
+
+
+    def parse(self, response):
+        # self.logger.info('Hi, this is an item page! %s', response.url)
+        product = ProductListItem()
+
+        # product['url'] = response.url
+	# prices = response.xpath('//*[@class="woocommerce-Price-currencySymbol"]/following-sibling::text()').extract()
+	# names = response.xpath('//*[@class="woocommerce-loop-product__title"]/text()').extract()
+	producturls = response.css('.woocommerce-LoopProduct-link::attr(href)').extract() 
+
+	# print(prices)
+	# print(names)
+	# print(producturls)
+
+	# print('Products on this page: %s',len(names))
+
+	for i in xrange(0,len(producturls)):
+	   # if len(prices) > i:
+           #    product['price'] = prices[i]
+	   # else:
+	   #     product['price'] = '0.00'
+           # product['name'] = names[i]
+           # if len(prices) > i:
+           product['producturl'] = producturls[i]
+	   #else:
+	   #    product['producturl'] = ''
+	   # self.logger.info('Processing:! %s', names[i])
+           yield product
+
+	next_page = response.css('.next::attr(href)').extract_first()
+        if next_page is not None:
+           next_page = response.urljoin(next_page)
+           yield scrapy.Request(next_page, callback=self.parse)
